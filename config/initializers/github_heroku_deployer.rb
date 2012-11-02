0e14142e1f763c975752fae1ebd5981d2ce68825
Jessica Lynn Suttles <jlsuttles@gmail.com>
2012-11-02 14:47:18 -0700

    fixes typo

--- config/initializers/github_heroku_deployer.rb
+++ config/initializers/github_heroku_deployer.rb
@@ -2,7 +2,7 @@ GithubHerokuDeployer.configure do |config|
   config.github_repo     = ENV["GITHUB_REPO"]
   config.heroku_api_key  = ENV["HEROKU_API_KEY"]
   config.heroku_app_name = ENV["HEROKU_APP_NAME"]
-  config.heroku_repo     = ENV["HEROKU_REPO_NAME"]
+  config.heroku_repo     = ENV["HEROKU_REPO"]
   config.heroku_username = ENV["HEROKU_USERNAME"]
   config.id_rsa          = ENV["ID_RSA"]
   config.logger          = Rails.logger
