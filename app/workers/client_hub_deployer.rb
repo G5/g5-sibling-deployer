e0201b1e186af694ba46fb06ad35ae9330f0527a
Jessica Lynn Suttles <jlsuttles@gmail.com>
2012-12-10 18:39:29 -0800

    rips out a lot of things

--- app/workers/client_hub_deployer.rb
+++
@@ -1,8 +0,0 @@
-class ClientHubDeployer
-  extend HerokuResqueAutoscaler if Rails.env.production?
-  @queue = :deployer
-
-  def self.perform
-    ClientHub.deploy
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ require 'resque/server'
 
 G5ClientHubDeployer::Application.routes.draw do
   post "deploy" => "client_hub#deploy", as: "deploy"
-  post "webhooks/g5-configurator" => "webhooks#g5_configurator"
 
   mount Resque::Server, :at => "/resque"
-  root to: "client_hub#index"
+  root to: "siblings#index"
 end
