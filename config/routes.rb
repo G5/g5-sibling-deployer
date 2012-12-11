3cddf71497cfe3f13792171708728cd6b0e6846a
Jessica Lynn Suttles <jlsuttles@gmail.com>
2012-12-10 20:33:46 -0800

    adds g5_sibling_deployer gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 require 'resque/server'
 
 G5ClientHubDeployer::Application.routes.draw do
-  post "deploy" => "client_hub#deploy", as: "deploy"
-
   mount Resque::Server, :at => "/resque"
   root to: "siblings#index"
 end
