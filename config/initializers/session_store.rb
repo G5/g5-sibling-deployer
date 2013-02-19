a78d311b95dbe4af57e8431782832d6008e307af
Michael Mitchell <michael.mitchell@g5searchmarketing.com>
2013-02-19 15:07:31 -0800

    changes app name to G5SiblingDeployer

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-G5ClientHubDeployer::Application.config.session_store :cookie_store, key: '_g5-client-hub-deployer_session'
+G5SiblingDeployer::Application.config.session_store :cookie_store, key: '_g5-client-hub-deployer_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# G5ClientHubDeployer::Application.config.session_store :active_record_store
+# G5SiblingDeployer::Application.config.session_store :active_record_store
