661de10173d7751dd6daf284b9f0940b9af7d606
Michael Mitchell <michael.mitchell@g5searchmarketing.com>
2013-02-21 17:27:16 -0800

    fix screwy migration issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121211022923) do
+ActiveRecord::Schema.define(:version => 20130222012640) do
 
   create_table "sibling_deploys", :force => true do |t|
     t.integer  "sibling_id"
