594381e9edfdd77cfab16477b2de652da1fc8a3d
Michael Mitchell <michael.mitchell@g5searchmarketing.com>
2013-02-21 16:17:35 -0800

    more name changes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-G5SiblingDeployer::Application.config.session_store :cookie_store, key: '_g5-client-hub-deployer_session'
+G5SiblingDeployer::Application.config.session_store :cookie_store, key: '_g5-sibling-deployer_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
