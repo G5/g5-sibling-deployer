96423f5c75de01300c74d9b211c38c0691958814
jessicard <jessicard@mac.com>
2014-03-03 11:44:19 -0800

    destroys sibling dependencies in ch -> cms migration

--- db/migrate/20140227232816_update_client_hub_to_cms.rb
+++ db/migrate/20140227232816_update_client_hub_to_cms.rb
@@ -1,6 +1,9 @@
 class UpdateClientHubToCms < ActiveRecord::Migration
   def up
     Sibling.destroy_all
+    Sibling::Deploy.destroy_all
+    Sibling::Instruction.destroy_all
+
     SiblingConsumer.perform
   end
 
