6d433182c113678c0d52ab5bfc09dede0d02d9c5
Steve Craig <steve.craig@getg5.com>
2015-06-30 11:57:53 -0700

    update applist, update dorelease to be asynch

--- dorelease.sh
+++ dorelease.sh
@@ -2,51 +2,57 @@
 
 # IMPORTANT: Always update applist.txt from https://g5-app-wrangler.herokuapp.com/apps.json
 
-# version="master"
+version="master"
 echo "Starting Release:"
 i=1
 while read p || [[ -n $p ]]; do
-    echo ; echo ;
-    echo "$i) Releasing to $p"
-    if [[ $p == *"#"* ]]
-    then
-        echo "Skipping $p"
-        i=$[$i+1]
-        continue
-    fi
+  echo ; echo ;
+  echo "$i) Releasing to $p"
+  if [[ $p == *"#"* ]]
+  then
+      echo "Skipping $p"
+      i=$[$i+1]
+      continue
+  fi
 
-    # echo "Stopping app..."
-    # heroku ps:stop web --app "$p"
+  # echo "Stopping app..."
+  # heroku ps:stop web --app "$p"
 
 #    echo "Removing worker and scheduler dynos"
 #    heroku ps:scale worker=0 --app "$p"
 #    heroku ps:scale scheduler=0 --app "$p"
 
-#    echo "Setting config"
-#    heroku config:set FURY_AUTH=msc9777J2TEEbgRsedKE -a "$p"
-    # heroku config:set G5_HUB=https://g5-hub.herokuapp.com -a "$p"
+#  echo "Setting config"
+  # heroku config:set HOST="https://$p.herokuapp.com" -a "$p"
+  # heroku config:set G5_HUB=https://g5-hub.herokuapp.com -a "$p"
+#  heroku config:set FURY_AUTH=msc9777J2TEEbgRsedKE -a "$p"
 
-    # echo "Unsetting config"
-    # heroku config:unset HOST -a "$p"
+#  echo "Adding addons"
+#  heroku addons:create deployhooks:http --url=https://$p.herokuapp.com/post_deploy_hook --app $p
 
-    echo "Pushing release"
-    git push -f git@heroku.com:$p.git $version:master > ./logs/$p.log 2>&1 &
+#  echo "Unsetting config"
+#  heroku config:unset HOST -a "$p"
+
+#  echo "Backing up database"
+#  heroku pg:backups capture -a $p
+
+  echo "Pushing release"
+  git push -f git@heroku.com:$p.git $version:master > ./logs/$p.log 2>&1 &
 
 #    heroku restart --app $p
 
-    # echo "Running migration..."
+  # echo "Running migration..."
+  # # We have to redirect stdin otherwise this command will consume the rest of the filenames
+  # exec 6<&0      # Link file descriptor #6 with stdin.
+  #                # Saves stdin.
+  # exec < /dev/null
+  # (heroku run rake db:migrate --app "$p" > ./logs/$p.log 2>&1 &)
+  # exec 0<&6 6<&- # restore stdin
 
-    # # We have to redirect stdin otherwise this command will consume the rest of the filenames
-    # exec 6<&0      # Link file descriptor #6 with stdin.
-    #                # Saves stdin.
-    # exec < /dev/null
-    # (heroku run rake db:migrate --app "$p")
-    # exec 0<&6 6<&- # restore stdin
+#  echo "Starting app..."
+#  heroku ps:restart --app "$p" > ./logs/$p.log 2>&1 &
 
-    # echo "Starting app..."
-    # heroku ps:restart --app "$p"
-
-    i=$[$i+1]
-done < applist5.txt
+  i=$[$i+1]
+done < $1
 
 echo "Done"
